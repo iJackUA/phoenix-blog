b93a3a9edede6cba985c563c33de849dd1f9691a
Yevhen Kuzminov <kyzminov@gmail.com>
2018-01-30 19:30:19 +0200

    chnage helpers

--- lib/blog_web/views/layout_view.ex
+++ lib/blog_web/views/layout_view.ex
@@ -2,18 +2,18 @@ defmodule BlogWeb.LayoutView do
   use BlogWeb, :view
 
   def js_script_tag(src) do
-    if Mix.env == :prod do
+    if Mix.env() == :prod do
       "<script src=\"#{src}\"></script>"
     else
-      ~s(<script src="http://localhost:8080/js/app.js"></script>)
+      "<script src=\"http://localhost:8080/#{src}\"></script>"
     end
   end
 
-  def css_link_tag do
-    if Mix.env == :prod do
-      ~s(<link rel="stylesheet" type="text/css" href="/css/app.css" media="screen,projection" />)
+  def css_link_tag(src) do
+    if Mix.env() == :prod do
+      "<link rel=\"stylesheet\" type=\"text/css\" href=\"#{src}\" media=\"screen,projection\" />"
     else
       ""
     end
-
+  end
 end
